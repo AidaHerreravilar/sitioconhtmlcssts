d921cf83268e99460106b99cc095573ba56b4717
AidaHerreravilar <92064121+AidaHerreravilar@users.noreply.github.com>
2022-04-09 22:55:22 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let nombre = prompt("escriba su nombre");
+
+function funcionSaludar() {
+  console.log("hola" + aida);
+}
+let botonSaludar = document.getElementById("saludar");
+botonSaludar.addEventListener("click", funcionSaludar);
