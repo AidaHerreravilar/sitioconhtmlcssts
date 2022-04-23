540aff90b36bb920dc50f337256a34f4ae79f719
AidaHerreravilar <92064121+AidaHerreravilar@users.noreply.github.com>
2022-04-23 20:29:19 -0300

    actualizado23deabril

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-let nombre = prompt("escriba su nombre");
-
-function funcionSaludar() {
-  console.log("hola" + aida);
+let precioUnitario: number[] = [145, 85, 100, 105, 210];
+let cargarProductos = () => {
+  for (let i =0; i < stock.length; i++) {
+    let divProducto = document.createElement("div");
+    divProducto.classList.add("item-producto");
+    divProducto.id = producto-${i};
+    let nombreProducto = document.createElement("p");
+    nombreProducto.innerHTML = ´${precioUnitario[i]}´;
+    let selectorCantidad = document.createElement("input");
+    selectorCantidad.id = ´selector-cantidad-${i}´;
+    selectorCantidad.type = "number";
+    selectorCantidad.max = String(stock[i]);
+    selectorCantidad.min = "0";
+    divProducto.appendChild(nombreProducto);
+    divProducto.appendChild(precioProducto);
+    divProducto.appendChild(selectorCantidad);
+    containerProductos.appendChild(divProducto);
+  }
+.productos {
+  color: rgb(202, 73, 13);
+  text-align: center;
+  padding: 30px;
+  margin: 30px;
 }
-let botonSaludar = document.getElementById("saludar");
-botonSaludar.addEventListener("click", funcionSaludar);
